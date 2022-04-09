0d6981d084da948198c091511807afd4188aaf48
ima365 <58730794+ima365@users.noreply.github.com>
2022-04-09 00:41:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*Write a simple function in JavaScript to loop through the first 200 numbers. If the number is
+divisible by 3, store it in a Fizz array. If the number is divisible by 5, store it in Buzz array, If the
+number is divisible by 3 and 5, store it in FizzBuzz array, otherwise store the number in Others.*/
+
 let Fizz = [ ];
 let Buzz = [ ];
 let FizzBuzz = [ ];
@@ -21,6 +25,18 @@ console.log("Total Number of FizzBuzz = "  + FizzBuzz.length);
 console.log("Total Number of Other = "    + Other.length);
 
 
+// write a simple function in JavaScript that will enable you to segment 100 Users equally using their User ID number.
 
+let oddUser = [ ];
+let evenUser = [ ];
 
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0 ) {
+        evenUser.push("idNo-" + i);
+    } else {
+        oddUser.push("idNo-" + i);
+    }
+}
 
+console.log(oddUser);
+console.log(evenUser);
